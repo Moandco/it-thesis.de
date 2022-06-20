3284b195e43cda7a405faa592a045135b9ad0290
Moritz Dinger <mail@moritz-dinger.de>
2022-06-20 20:58:02 +0200

    Enable email capture

--- newsletter.js
+++ newsletter.js
@@ -5,11 +5,11 @@ function submitMail(event) {
     var email = email_input.value
     email_input.disabled = true
     button.disabled = true
-    /*fetch('https://it-thesis.us13.list-manage.com/subscribe/post-json?' + new URLSearchParams({
+    fetch('https://it-thesis.us13.list-manage.com/subscribe/post-json?' + new URLSearchParams({
         u: '38d4a003677139c8d70ce7e74',
         id: '86ac8bc96a',
         EMAIL: email
-    }))*/
+    }))
 }
 
 function studentMail(event) {
